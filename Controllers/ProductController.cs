0d66b8cfcc60dc46a3600b71c943f95105db51dc
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2025-01-20 14:19:48 +0100

    x

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StrikkebutikkBackend.Model;
+using System.Data.SqlClient;
+
+namespace StrikkebutikkBackend.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+
+        [HttpGet(Name = "GetProduct")]
+        public IEnumerable<Product> GetProduct()
+        {
+
+            return [new Product { Id = 0, ProductName = "Seven Sisters - genser" },
+                    new Product {Id = 1, ProductName = "Bobbie"}];
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace StrikkebutikkBackend.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
+
+
+/*
+ *  id: 0,
+        productImg: "source/img/seven-sister-genser.png",
+        productAlbum: [
+          "source/img/seven-sisters1.png",
+          "source/img/seven-sisters2.png",
+          "source/img/seven-sisters3.png",
+        ],
+        productName: "Seven Sisters - genser",
+        sizes: ["s", "m", "l"],
+        category: "genser",
+        patternId: 0,
+        quantity: 2,
+        price: 799,
+        assortmentId: 0,
+        productInfo:
+          "Genseren strikkes sømløst ovenfra og ned og har et grafisk rutemønstersom minner om det ikoniskerutemønsteret som finnes på setene på t-banen i London. Det fremre og bakre bærestykket, samt raglanermene, strikkes samtidig i ett stykke for å oppnå nøyaktig samme mål.",
+      },
+*/
